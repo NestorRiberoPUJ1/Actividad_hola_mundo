ed56ed44a995d092e26fb30f3a0d1030ce61a217
Francisco Carlos Calderon <nestor_05ribero@hotmail.com>
2022-02-28 12:11:29 -0500

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,11 @@
+print("Hola Mundo")
+Nombre= "Nestor"
+print(f"¡Hola, {Nombre}!")
+print("¡Hola, "+Nombre+"!")
+Numero = 24
+print("¡Hola, ",Numero,"!")
+print("¡Hola, "+str(Numero)+"!")
+comida1="Tacos"
+comida2="Enchiladas"
+print("Amo comer {} y {}".format(comida1,comida2))
+print(f"Amo comer {comida1} y {comida2}")
